a424fe420309f637d3776d580f58bff7ddc995ba
Sunan13208 <sunanrabbi1918@gmail.com>
2024-03-19 20:35:38 +0600

    not found route

--- src/app/middleWare/notFound.ts
+++ src/app/middleWare/notFound.ts
@@ -5,7 +5,10 @@ const notFound = (req: Request, res: Response, next: NextFunction) => {
     return res.status(httpStatus.NOT_FOUND).json({
         success: false,
         message: 'Give a valid route!!!',
-        error: 'API NOT FOUND'
+        error: {
+            path: req.originalUrl,
+            message: 'API NOT FOUND'
+        },
     })
 }
 
